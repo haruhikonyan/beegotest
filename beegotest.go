ccf2931a2cebeb30b1da4e1af7c0788d42d50152
Otani Haruhiko <haruhiko@weseek.co.jp>
2018-09-19 10:15:49 +0900

    初期起動まで

---
+++ beegotest.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "Hello, 世界"
+	fmt.Println(hello)
+}
---
+++ haruch/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "beegotest/haruch/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
